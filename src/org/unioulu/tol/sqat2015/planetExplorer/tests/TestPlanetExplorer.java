2ee36e6382c24bedc26dced157d432ae34cc31b7
somename <someemail>
2015-10-30 16:02:49 +0200

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -2,22 +2,33 @@ package org.unioulu.tol.sqat2015.planetExplorer.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
+	PlanetExplorer explorerNoObs;
+	@Before
+	public void setup(){
+		explorerNoObs = new PlanetExplorer(100,100,"");
+	}
 
 	@Test
 	public void testGetPositionStartingPosition_return0_0() {
-		PlanetExplorer explorer = new PlanetExplorer(100,100,"");
-		String position = explorer.getPosition();
+		
+		String position = explorerNoObs.getPosition();
 		assertEquals("0,0", position);
 	}
 	
 	@Test
 	public void testGetFacingStartingPosition_returnN(){
-		PlanetExplorer explorer = new PlanetExplorer(100,100,"");
-		char facing = explorer.getFacing();
+		PlanetExplorer explorerNoObs = new PlanetExplorer(100,100,"");
+		char facing = explorerNoObs.getFacing();
 		assertEquals('N', facing);
 	}
+	@Test
+	public void testTurn_Right_ReturnW(){
+		
+	}
+	
 }
