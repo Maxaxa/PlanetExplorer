32ad0a2a9bec88919af4aeb33bf64ce30526d70b
somename <someemail>
2015-10-30 15:38:33 +0200

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -3,11 +3,14 @@ package org.unioulu.tol.sqat2015.planetExplorer.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetPositionStartposition_return0_0() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"");
+		String position = explorer.getPosition();
+		assertEquals("0,0", position);
 	}
 }
