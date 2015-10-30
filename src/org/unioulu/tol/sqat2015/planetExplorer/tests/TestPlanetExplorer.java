2419b1120ef1fce38de4c89948e369fb83ded20b
somename <someemail>
2015-10-30 15:44:33 +0200

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -10,7 +10,7 @@ public class TestPlanetExplorer {
 	@Test
 	public void testGetPositionStartposition_return0_0() {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"");
-		String position = explorer.getPosition();
+		String position = explorer.getPosition(explorer);
 		assertEquals("0,0", position);
 	}
 }
